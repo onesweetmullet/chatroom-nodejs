50ec16acb851c86cfdb9332a056d3382561e44c5
Peter Burke <pcburke@gmail.com>
2015-10-12 14:23:35 -0400

    using app.js instead of index.js

---
+++ app.js
@@ -0,0 +1,50 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var os = require('os');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    msg.ipAddress = this.handshake.address;
+    msg.datestamp = new Date();
+    
+    fs.appendFile('history.txt', JSON.stringify(msg) + os.EOL, function(err) {
+        
+    });
+    
+    console.log('message: ' + msg.username + " - " + msg.message);
+    
+    io.emit('chat message', msg);
+  });
+  
+  
+  fs.readFile('history.txt', function(err, data) {
+    
+    var _data = String.fromCharCode.apply(null, data);
+    var _lines = _data.split(os.EOL);
+    
+    var _historyObject = {};
+    if (_lines) {
+      for (var i = 0; i < _lines.length; i++) {
+        try {
+          var _msg = JSON.parse(_lines[i]);
+          _historyObject[i] = _msg;  
+        } catch (err) {
+          // do nothing
+        }
+                
+      }
+    }
+    
+    socket.emit('history-onload', _historyObject);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
